55748d88f4efde94de2fd010b3efeccf64312f20
André Relva <up202108695@up.pt>
2022-10-11 11:37:20 +0100

    Reimplemented stubs using Mockito

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -3,6 +3,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -41,15 +42,10 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        class StubListDeduplicator implements GenericListDeduplicator {
-            @Override
-            public List<Integer> deduplicate(List<Integer> list) {
-                return Arrays.asList(1, 2, 4, 5);
-            }
-        }
 
         ListAggregator aggregator = new ListAggregator();
-        StubListDeduplicator deduplicator = new StubListDeduplicator();
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4, 5));
         int distinct = aggregator.distinct(list, deduplicator);
 
         Assertions.assertEquals(4, distinct);
@@ -66,16 +62,10 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct_bug_8726() {
-        class StubListDeduplicator implements GenericListDeduplicator {
-            @Override
-            public List<Integer> deduplicate(List<Integer> list) {
-                return Arrays.asList(1, 2, 4);
-            }
-        }
-
         List<Integer> list = Arrays.asList(1, 2, 4, 2);
         ListAggregator aggregator = new ListAggregator();
-        StubListDeduplicator deduplicator = new StubListDeduplicator();
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
         int distinct = aggregator.distinct(list, deduplicator);
 
         Assertions.assertEquals(3, distinct);
--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -3,6 +3,8 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,14 +22,8 @@ public class ListDeduplicatorTest {
 
     @Test
     public void deduplicate() {
-        class StubListSorter implements GenericListSorter {
-            @Override
-            public List<Integer> sort(List<Integer> list) {
-                return Arrays.asList(1, 2, 2, 4, 5);
-            }
-        }
-
-        StubListSorter sorter = new StubListSorter();
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 2, 4, 5));
         ListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(list);
         Assertions.assertEquals(expected, distinct);
@@ -37,15 +33,8 @@ public class ListDeduplicatorTest {
     public void deduplicate_bug_8726() {
         List<Integer> list = Arrays.asList(1, 2, 4, 2);
         List<Integer> expected = Arrays.asList(1, 2, 4);
-
-        class StubListSorter implements GenericListSorter {
-            @Override
-            public List<Integer> sort(List<Integer> list) {
-                return Arrays.asList(1, 2, 2, 4);
-            }
-        }
-
-        StubListSorter sorter = new StubListSorter();
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 2, 4));
         ListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(list);
         Assertions.assertEquals(expected, distinct);
