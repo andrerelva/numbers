67fd76168348b217359fc2bf64044b077a0b7444
André Relva <up202108695@up.pt>
2022-10-11 11:25:12 +0100

    Improper test fixed (messed up code when pasting)

--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -36,7 +36,7 @@ public class ListDeduplicatorTest {
     @Test
     public void deduplicate_bug_8726() {
         List<Integer> list = Arrays.asList(1, 2, 4, 2);
-        List<Integer> expected = Arrays.asList(1, 2, 2, 4);
+        List<Integer> expected = Arrays.asList(1, 2, 4);
 
         class StubListSorter implements GenericListSorter {
             @Override
@@ -47,6 +47,7 @@ public class ListDeduplicatorTest {
 
         StubListSorter sorter = new StubListSorter();
         ListDeduplicator deduplicator = new ListDeduplicator(sorter);
-        Assertions.assertEquals(expected, expected);
+        List<Integer> distinct = deduplicator.deduplicate(list);
+        Assertions.assertEquals(expected, distinct);
     }
 }
